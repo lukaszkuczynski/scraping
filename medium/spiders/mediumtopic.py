be98772fe3e7b40995d888b79064061fdd3df939
Lukasz Kuczynski <l.kuczynski@exlabs.pl>
2020-05-05 10:57:49 +0200

    Only latest posts included

--- medium/spiders/mediumtopic.py
+++ medium/spiders/mediumtopic.py
@@ -2,8 +2,6 @@
 import scrapy
 from bs4 import BeautifulSoup   
 
-MAX_HEADERS = 8
-
 class MediumtopicSpider(scrapy.Spider):
     name = 'mediumtopic'
     topic = ""
@@ -13,12 +11,21 @@ class MediumtopicSpider(scrapy.Spider):
 
     def parse(self, response):
         div_articles = response.selector.xpath("/html/body/div/div/div[3]/div/div/div[1]/div[4]/div[1]")
-        h3_sel = div_articles.xpath("//h3")
-        for h3_html in h3_sel.getall()[:MAX_HEADERS]:
-            soup = BeautifulSoup(h3_html, 'html.parser')
+        titles_sel = div_articles.xpath("//h3[contains(@class, 'ap')]")
+        # for titles: we should have 12 elements, 1 main, 3 latest, 1 author, 7 other
+        titles = titles_sel.getall()[1:4]
+        desc_sel = div_articles.xpath("//h3[contains(@class, 'gc')]")                                                                                                                                      
+        # for descriptions: we should have 14 elements, 1 main - doubled!, 3 latest, 2 authors, 7 other
+        descriptions = desc_sel.getall()[2:5]
+        latest = zip(titles, descriptions)
+
+        for title, desc in latest:
+            title_soup = BeautifulSoup(title, 'html.parser')
+            desc_soup = BeautifulSoup(desc, 'html.parser')
             yield {
-                "header": soup.h3.text,
-                "link": soup.a['href']
+                "title": title_soup.h3.text,
+                "description": desc_soup.h3.text,
+                "link": title_soup.a['href']
             }
 
 
@@ -35,4 +42,3 @@ if __name__ == "__main__":
     h3_sel = div_articles.xpath("//h3")
     h3_html = h3_sel.get()
     soup = BeautifulSoup(h3_html, 'html.parser')
-
